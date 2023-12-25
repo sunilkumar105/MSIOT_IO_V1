36eea8690f5a2284068f44712745ecbd6b7a70dc
sunilkumar105 <sunilkumar62394@gmail.com>
2023-12-25 14:52:23 +0530

    initial commit, input and output working

---
+++ MSIO_IO_V_1/Core/Inc/main.h
@@ -0,0 +1,87 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_PC_13_Pin GPIO_PIN_13
+#define LED_PC_13_GPIO_Port GPIOC
+#define ADC_0_Pin GPIO_PIN_0
+#define ADC_0_GPIO_Port GPIOA
+#define U1_74HC_S3_Pin GPIO_PIN_1
+#define U1_74HC_S3_GPIO_Port GPIOA
+#define U1_74HC_S0_Pin GPIO_PIN_11
+#define U1_74HC_S0_GPIO_Port GPIOB
+#define U1_74HC_S1_Pin GPIO_PIN_12
+#define U1_74HC_S1_GPIO_Port GPIOB
+#define U1_74HC_S2_Pin GPIO_PIN_15
+#define U1_74HC_S2_GPIO_Port GPIOA
+#define RELAY_3_Pin GPIO_PIN_3
+#define RELAY_3_GPIO_Port GPIOB
+#define RELAY_2_Pin GPIO_PIN_4
+#define RELAY_2_GPIO_Port GPIOB
+#define RELAY_1_Pin GPIO_PIN_5
+#define RELAY_1_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ MSIO_IO_V_1/Core/Inc/utility.h
@@ -0,0 +1,14 @@
+/*
+ * utility.h
+ *
+ *  Created on: Dec 25, 2023
+ *      Author: sunil
+ */
+
+#ifndef INC_UTILITY_H_
+#define INC_UTILITY_H_
+#include "main.h"
+
+void Choose_Channel(uint8_t Channel_Num);
+
+#endif /* INC_UTILITY_H_ */
