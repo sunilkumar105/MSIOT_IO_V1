3b6b750904c1e6b4a1949ff98409c00f7d475371
sunilkumar105 <sunilkumar62394@gmail.com>
2023-12-26 22:20:23 +0530

    Partially reading and writing data, need to add feedback also

--- MSIO_IO_V_1/Core/Inc/utility.h
+++ MSIO_IO_V_1/Core/Inc/utility.h
@@ -18,14 +18,16 @@
 #define Debug_Data_Port huart1
 
 enum COMMAND_LIST {
-	COMMAND_WRITE_GPIO_DIGITAL = 0,
-	COMMAND_WRITE_ANALOG = 1,
-	READ_GPIO_DIGITAL = 2,
-	READ_ANALOG_ANALOG = 3
+	COMMAND_WRITE_GPIO_ANALOG = 1,
+	COMMAND_READ_GPIO_DIGITAL = 2,
+	COMMAND_READ_GPIO_ANALOG = 3,
+	COMMAND_WRITE_RELAYS = 4
 };
 
+uint8_t Get_GPIO_Num_For_Actuation(char received_data_from_gateway[]);
 void Print_Debug_Data(char *Debug_Msg);
 void Choose_Channel(uint8_t Channel_Num);
 void Manage_NRF_Data(void);
-
+uint8_t Read_Gpio_Digital(uint8_t GPIO_num);
+uint32_t Read_GPIO_ANALOG(uint8_t GPIO_num);
 #endif /* INC_UTILITY_H_ */
--- MSIO_IO_V_1/Core/Src/utility.c
+++ MSIO_IO_V_1/Core/Src/utility.c
@@ -10,6 +10,7 @@
 
 char my_node_id[2] = "01";
 extern UART_HandleTypeDef huart1;
+extern ADC_HandleTypeDef hadc1;
 
 void Print_Debug_Data(char *Debug_Msg) {
 	HAL_UART_Transmit(&huart1, (uint8_t*) Debug_Msg, strlen(Debug_Msg), 100);
@@ -76,60 +77,117 @@ void Choose_Channel(uint8_t Channel_Num) {
 void Manage_NRF_Data(void) {
 	char received_data_from_gateway[50] = "";
 	Print_Debug_Data("\n DATA RECEIVED FROM NRF::>> ");
-	NRF24_read(received_data_from_gateway, 5); //store received data
+	NRF24_read(received_data_from_gateway, 6); //store received data
 	Print_Debug_Data(received_data_from_gateway);
 
 	/*
 	 //check the command and ACT accordingly
 	 //	COMMAND LIST
-	 0 = WRITE GPIO
+	 0 = WRITE GPIO DIGITAL
 	 1 = WRITE ANALOG
-	 2 = READ GPIO
+	 2 = READ GPIO DIGITAL
 	 3 = READ ANALOG
+	 4 = WRITE RELAYS
 	 */
-
 	char rcvd_node_id[2];
 	char rcvd_command;
-	memcpy(rcvd_node_id, received_data_from_gateway, 2);
-	rcvd_command = received_data_from_gateway[2];
+	rcvd_command = (int) received_data_from_gateway[2];
 
+	memcpy(rcvd_node_id, received_data_from_gateway, 2);
 	//============CHECK IF NODE MATCH WITH ID======================//
 	if (!(memcmp(rcvd_node_id, my_node_id, 2))) {
 		Print_Debug_Data("\n NODE ID MATCHED");
 		Print_Debug_Data("\n COMMAND ::> ");
-		Print_Debug_Data(rcvd_command);
-	}
-
-	else {
+		Print_Debug_Data((char*) rcvd_command);
+	} else {
 		return;
+		Print_Debug_Data("NODE ID DOESNT MATCH");
+	}
+	uint8_t Actutate_GPIO = Get_GPIO_Num_For_Actuation(
+			received_data_from_gateway);
+	GPIO_PinState GPIO_STATE;
+	uint8_t GPIO_STATE_INT = (int) received_data_from_gateway[5];
+	if (GPIO_STATE_INT) {
+
+		GPIO_STATE = GPIO_PIN_SET;
+	} else if (!GPIO_STATE_INT) {
+
+		GPIO_STATE = GPIO_PIN_RESET;
+	} else {
+		Print_Debug_Data("\nERROR GATING GPIO STATE");
 	}
 
-	if (rcvd_command == COMMAND_WRITE_GPIO_DIGITAL) {
-		char GPIO_TO_WRITE[2];
-		GPIO_TO_WRITE[0] = received_data_from_gateway[3];
-		GPIO_TO_WRITE[1] = received_data_from_gateway[4];
-		int value = atoi(GPIO_TO_WRITE);
-		//SWITCH ON THE BASE OF GPIO NUM WRITE GPIO DIGITAL
-	}
-	if (rcvd_command == COMMAND_WRITE_ANALOG) {
-		char GPIO_TO_WRITE[2];
-		GPIO_TO_WRITE[0] = received_data_from_gateway[3];
-		GPIO_TO_WRITE[1] = received_data_from_gateway[4];
-		int value = atoi(GPIO_TO_WRITE);
+	//===========CHECK FOR COMMAND AND ACT ACCORDINGLY=====//
+	if (rcvd_command == COMMAND_WRITE_GPIO_ANALOG) {
+
 		//SWITCH ON THE BASE OF GPIO NUM WRITE GPIO ANALOG
+		if (!((Actutate_GPIO == 1) || (Actutate_GPIO == 0))) {
+			Print_Debug_Data("\n Wrong GPIOI NUM FOR ANALOG WRITE");
+			//TRY WRITING ANALOG DATA HERE WITH PWM / ANALOG IC
+		}
 	}
-	if (rcvd_command == READ_GPIO_DIGITAL) {
-		char GPIO_TO_WRITE[2];
-		GPIO_TO_WRITE[0] = received_data_from_gateway[3];
-		GPIO_TO_WRITE[1] = received_data_from_gateway[4];
-		int value = atoi(GPIO_TO_WRITE);
+	if (rcvd_command == COMMAND_READ_GPIO_DIGITAL) {
 		//SWITCH ON THE BASE OF GPIO NUM READ GPIO DIGITAL
+		uint8_t GPIO_STATE = Read_Gpio_Digital(Actutate_GPIO);
 	}
-	if (rcvd_command == READ_ANALOG_ANALOG) {
-		char GPIO_TO_WRITE[2];
-		GPIO_TO_WRITE[0] = received_data_from_gateway[3];
-		GPIO_TO_WRITE[1] = received_data_from_gateway[4];
-		int value = atoi(GPIO_TO_WRITE);
+
+	if (rcvd_command == COMMAND_READ_GPIO_ANALOG) {
 		//SWITCH ON THE BASE OF GPIO NUM READ GPIO ANALOG
+		uint32_t Analog_Value = Read_GPIO_ANALOG(Actutate_GPIO);
 	}
+	if (rcvd_command == COMMAND_WRITE_RELAYS) {
+		//SWITCH ON THE BASE OF GPIO NUM READ GPIO ANALOG
+		switch (Actutate_GPIO) {
+		case 1:
+			HAL_GPIO_WritePin(RELAY_1_GPIO_Port, RELAY_1_Pin, GPIO_STATE);
+			break;
+		case 2:
+			HAL_GPIO_WritePin(RELAY_2_GPIO_Port, RELAY_2_Pin, GPIO_STATE);
+			break;
+		case 3:
+			HAL_GPIO_WritePin(RELAY_3_GPIO_Port, RELAY_3_Pin, GPIO_STATE);
+			break;
+		default:
+			break;
+		}
+	}
+}
+/*
+ @brief This function will take two chracter array and will return the gpio num
+ */
+uint8_t Get_GPIO_Num_For_Actuation(char received_data_from_gateway[]) {
+	char GPIO_TO_WRITE[2];
+	GPIO_TO_WRITE[0] = received_data_from_gateway[3];
+	GPIO_TO_WRITE[1] = received_data_from_gateway[4];
+	int GPIO_Num_For_Actuation = atoi(GPIO_TO_WRITE);
+	return GPIO_Num_For_Actuation;
+}
+
+/*
+ @brief This function will read the (Digital) GPIO number of input series and will return its current state
+ */
+uint8_t Read_Gpio_Digital(uint8_t GPIO_num) {
+	Choose_Channel(GPIO_num);  //SELECT THE MUX
+	//	Read Its Digital State
+	uint32_t ADC_VAL;
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 10);
+	ADC_VAL = HAL_ADC_GetValue(&hadc1);
+	HAL_ADC_Stop(&hadc1);
+//The adc is of 12 bit, so the maximum value it can read is the 4096
+	if (ADC_VAL > 3600) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+uint32_t Read_GPIO_ANALOG(uint8_t GPIO_num) {
+	Choose_Channel(GPIO_num);  //SELECT THE MUX
+	//	Read Its Digital State
+	uint32_t ADC_VAL;
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 10);
+	ADC_VAL = HAL_ADC_GetValue(&hadc1);
+	HAL_ADC_Stop(&hadc1);
+	return ADC_VAL;
 }
